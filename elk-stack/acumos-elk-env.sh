98dbc7d7c88f187301a0204e1c29adc7f32db687
Sumit Kaushik <SK00613223@TechMahindra.com>
2020-08-19 20:42:44 +0530

    Upgrade OAM to 4.0.5
    ISSUE-ID :ACUMOS-4265
    
    Upgrade OAM to 4.0.5
    
    Change-Id: Id51d534ffafb52e181bc2607285176d649bb03bf
    Signed-off-by: Sumit Kaushik <SK00613223@TechMahindra.com>
    Signed-off-by: sk00613223 <sk00613223@techmahindra.com>

--- elk-stack/acumos-elk-env.sh
+++ elk-stack/acumos-elk-env.sh
@@ -51,7 +51,7 @@ export NEXUS3_STAGING_REGISTRY_LF=nexus3.acumos.org:10004
 export NEXUS3_RELEASE_REGISTRY_LF=nexus3.acumos.org:10002
 
 # Images
-export ELASTICSEARCH_IMAGE=acumos-elasticsearch:4.0.4
-export LOGSTASH_IMAGE=acumos-logstash:4.0.4
-export KIBANA_IMAGE=acumos-kibana:4.0.4
-export ELK_CLIENT=elk-client:4.0.4
+export ELASTICSEARCH_IMAGE=acumos-elasticsearch:4.0.5
+export LOGSTASH_IMAGE=acumos-logstash:4.0.5
+export KIBANA_IMAGE=acumos-kibana:4.0.5
+export ELK_CLIENT=elk-client:4.0.5
